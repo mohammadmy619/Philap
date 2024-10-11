482398eeec6fc9c3db798363c73b0a217c76e1ae
mohammad yousefi <mohammadmy619@gmail.com>
2024-10-11 13:38:42 +0330

    Create Accommodation Template

--- Src/AppHost/Aspire.AppHost/Aspire.AppHost.AppHost/Program.cs
+++ Src/AppHost/Aspire.AppHost/Aspire.AppHost.AppHost/Program.cs
@@ -31,4 +31,26 @@ builder.AddProject<Projects.Shipment_Api>("shipment-api");
 //    .WithReference(cache)
 //    .WithReference(apiService);
 
+builder.AddProject<Projects.Person_Api>("person-api");
+
+//var cache = builder.AddRedis("cache");
+
+//var apiService = builder.AddProject<Projects.Aspire_AppHost_ApiService>("apiservice");
+
+//builder.AddProject<Projects.Aspire_AppHost_Web>("webfrontend")
+//    .WithExternalHttpEndpoints()
+//    .WithReference(cache)
+//    .WithReference(apiService);
+
+builder.AddProject<Projects.Accommadation_Api>("accommadation-api");
+
+//var cache = builder.AddRedis("cache");
+
+//var apiService = builder.AddProject<Projects.Aspire_AppHost_ApiService>("apiservice");
+
+//builder.AddProject<Projects.Aspire_AppHost_Web>("webfrontend")
+//    .WithExternalHttpEndpoints()
+//    .WithReference(cache)
+//    .WithReference(apiService);
+
 builder.Build().Run();
