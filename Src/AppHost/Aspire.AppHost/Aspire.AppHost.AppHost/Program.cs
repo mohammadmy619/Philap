239038fd6f30cee6eef003fd44baaa51ebbeb1c8
mohammad yousefi <mohammadmy619@gmail.com>
2024-10-11 13:25:13 +0330

    Create Shipment project

--- Src/AppHost/Aspire.AppHost/Aspire.AppHost.AppHost/Program.cs
+++ Src/AppHost/Aspire.AppHost/Aspire.AppHost.AppHost/Program.cs
@@ -20,4 +20,15 @@ builder.AddProject<Projects.Ocelot_ApiGateways>("ocelot-apigateways");
 //    .WithReference(cache)
 //    .WithReference(apiService);
 
+builder.AddProject<Projects.Shipment_Api>("shipment-api");
+
+//var cache = builder.AddRedis("cache");
+
+//var apiService = builder.AddProject<Projects.Aspire_AppHost_ApiService>("apiservice");
+
+//builder.AddProject<Projects.Aspire_AppHost_Web>("webfrontend")
+//    .WithExternalHttpEndpoints()
+//    .WithReference(cache)
+//    .WithReference(apiService);
+
 builder.Build().Run();
