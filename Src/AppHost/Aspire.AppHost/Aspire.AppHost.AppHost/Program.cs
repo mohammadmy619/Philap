0eb2d9b2acc7483231210696296c0e3a3c993a9c
mohammad yousefi <mohammadmy619@gmail.com>
2024-10-11 13:12:50 +0330

    Create ApiGateWays  template

--- Src/AppHost/Aspire.AppHost/Aspire.AppHost.AppHost/Program.cs
+++ Src/AppHost/Aspire.AppHost/Aspire.AppHost.AppHost/Program.cs
@@ -9,4 +9,15 @@ var builder = DistributedApplication.CreateBuilder(args);
 //    .WithReference(cache)
 //    .WithReference(apiService);
 
+builder.AddProject<Projects.Ocelot_ApiGateways>("ocelot-apigateways");
+
+//var cache = builder.AddRedis("cache");
+
+//var apiService = builder.AddProject<Projects.Aspire_AppHost_ApiService>("apiservice");
+
+//builder.AddProject<Projects.Aspire_AppHost_Web>("webfrontend")
+//    .WithExternalHttpEndpoints()
+//    .WithReference(cache)
+//    .WithReference(apiService);
+
 builder.Build().Run();
