7f6b6f85a8e09bfea3d4878b3a97138efe4dc6a3
mohammad yousefi <mohammadmy619@gmail.com>
2025-02-05 23:15:28 +0330

    add Authentication service

--- Src/AppHost/Aspire.AppHost/Aspire.AppHost.AppHost/Program.cs
+++ Src/AppHost/Aspire.AppHost/Aspire.AppHost.AppHost/Program.cs
@@ -64,4 +64,26 @@ builder.AddProject<Projects.Ticketing_Api>("ticketing-api");
 //    .WithReference(cache)
 //    .WithReference(apiService);
 
+builder.AddProject<Projects.Authentication_Api>("authentication-api");
+
+//var cache = builder.AddRedis("cache");
+
+//var apiService = builder.AddProject<Projects.Aspire_AppHost_ApiService>("apiservice");
+
+//builder.AddProject<Projects.Aspire_AppHost_Web>("webfrontend")
+//    .WithExternalHttpEndpoints()
+//    .WithReference(cache)
+//    .WithReference(apiService);
+
+builder.AddProject<Projects.Infrastructure>("infrastructure");
+
+//var cache = builder.AddRedis("cache");
+
+//var apiService = builder.AddProject<Projects.Aspire_AppHost_ApiService>("apiservice");
+
+//builder.AddProject<Projects.Aspire_AppHost_Web>("webfrontend")
+//    .WithExternalHttpEndpoints()
+//    .WithReference(cache)
+//    .WithReference(apiService);
+
 builder.Build().Run();
