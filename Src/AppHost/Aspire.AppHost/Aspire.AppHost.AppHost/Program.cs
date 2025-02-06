cff582469297786efd2445bbca86e00e58e2fd6c
mohammad yousefi <mohammadmy619@gmail.com>
2025-02-06 14:51:36 +0330

    fix launchsettings

--- Src/AppHost/Aspire.AppHost/Aspire.AppHost.AppHost/Program.cs
+++ Src/AppHost/Aspire.AppHost/Aspire.AppHost.AppHost/Program.cs
@@ -1,13 +1,6 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-//var cache = builder.AddRedis("cache");
 
-//var apiService = builder.AddProject<Projects.Aspire_AppHost_ApiService>("apiservice");
-
-//builder.AddProject<Projects.Aspire_AppHost_Web>("webfrontend")
-//    .WithExternalHttpEndpoints()
-//    .WithReference(cache)
-//    .WithReference(apiService);
 
 builder.AddProject<Projects.Ocelot_ApiGateways>("ocelot-apigateways");
 
@@ -20,81 +13,19 @@ builder.AddProject<Projects.Ocelot_ApiGateways>("ocelot-apigateways");
 //    .WithReference(cache)
 //    .WithReference(apiService);
 
-builder.AddProject<Projects.Shipment_Api>("shipment-api");
-
-//var cache = builder.AddRedis("cache");
-
-//var apiService = builder.AddProject<Projects.Aspire_AppHost_ApiService>("apiservice");
-
-//builder.AddProject<Projects.Aspire_AppHost_Web>("webfrontend")
-//    .WithExternalHttpEndpoints()
-//    .WithReference(cache)
-//    .WithReference(apiService);
-
 builder.AddProject<Projects.Person_Api>("person-api");
 
-//var cache = builder.AddRedis("cache");
 
-//var apiService = builder.AddProject<Projects.Aspire_AppHost_ApiService>("apiservice");
 
-//builder.AddProject<Projects.Aspire_AppHost_Web>("webfrontend")
-//    .WithExternalHttpEndpoints()
-//    .WithReference(cache)
-//    .WithReference(apiService);
-
-builder.AddProject<Projects.Accommadation_Api>("accommadation-api");
-
-//var cache = builder.AddRedis("cache");
-
-//var apiService = builder.AddProject<Projects.Aspire_AppHost_ApiService>("apiservice");
-
-//builder.AddProject<Projects.Aspire_AppHost_Web>("webfrontend")
-//    .WithExternalHttpEndpoints()
-//    .WithReference(cache)
-//    .WithReference(apiService);
 
 builder.AddProject<Projects.Ticketing_Api>("ticketing-api");
 
-//var cache = builder.AddRedis("cache");
 
-//var apiService = builder.AddProject<Projects.Aspire_AppHost_ApiService>("apiservice");
-
-//builder.AddProject<Projects.Aspire_AppHost_Web>("webfrontend")
-//    .WithExternalHttpEndpoints()
-//    .WithReference(cache)
-//    .WithReference(apiService);
 
 builder.AddProject<Projects.Authentication_Api>("authentication-api");
 
-//var cache = builder.AddRedis("cache");
-
-//var apiService = builder.AddProject<Projects.Aspire_AppHost_ApiService>("apiservice");
-
-//builder.AddProject<Projects.Aspire_AppHost_Web>("webfrontend")
-//    .WithExternalHttpEndpoints()
-//    .WithReference(cache)
-//    .WithReference(apiService);
-
-builder.AddProject<Projects.Infrastructure>("infrastructure");
-
-//var cache = builder.AddRedis("cache");
-
-//var apiService = builder.AddProject<Projects.Aspire_AppHost_ApiService>("apiservice");
-
-//builder.AddProject<Projects.Aspire_AppHost_Web>("webfrontend")
-//    .WithExternalHttpEndpoints()
-//    .WithReference(cache)
-//    .WithReference(apiService);
 
 builder.AddProject<Projects.Trip_Api>("trip-api");
 
-//var cache = builder.AddRedis("cache");
-
-//var apiService = builder.AddProject<Projects.Aspire_AppHost_ApiService>("apiservice");
-
-//builder.AddProject<Projects.Aspire_AppHost_Web>("webfrontend")
-//    .WithExternalHttpEndpoints()
-//    .WithReference(cache)
-//    .WithReference(apiService);
 
 builder.Build().Run();
