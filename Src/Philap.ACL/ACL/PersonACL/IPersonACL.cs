2a726c04f3e5ff67d1a987cdd4fbe1ba17a2e65e
Mohammad <mohammadmy619@gmail.com>
2026-05-28 09:16:37 +0330

    Create sample Acl  for person data

---
+++ Src/Philap.ACL/ACL/PersonACL/IPersonACL.cs
@@ -0,0 +1,21 @@
+﻿using BuildingBlocks.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACL.PersonACL
+{
+    public interface IPersonACL: IAcl
+    {
+
+        /// <summary>
+        /// بررسی می‌کند آیا Leader در ساب‌دامین مبدا وجود دارد و وضعیت Active دارد
+        /// </summary>
+        Task<bool> IsLeaderValidAsync(Guid leaderId, CancellationToken cancellationToken);
+
+
+
+    }
+}
---
+++ Src/Shared/BuildingBlocks/Interface/IAcl.cs
@@ -0,0 +1,6 @@
+﻿namespace BuildingBlocks.Interface
+{
+    public interface IAcl
+    {
+    }
+}
