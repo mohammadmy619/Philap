dfda1d8b5a6e54985113e2438f3df5f56362597f
Mohammad <mohammadmy619@gmail.com>
2026-07-26 00:15:26 +0330

    Implement  Sample domain service

---
+++ Src/Services/Ticket/Core/Domain/DomainServices/ICalculateBookingPricing.cs
@@ -0,0 +1,20 @@
+﻿using BuildingBlocks.Domain;
+using Domain.BookingAggregate;
+using Domain.DiscountAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.DomainServices
+{
+    public interface ICalculateBookingPricing
+    {
+       PricingResult ProcessBookingPriceDiscount(Booking booking, Discount? discount);
+
+       //string GetCurrency(Money price);
+
+    }
+}
+
---
+++ Src/Services/Ticket/Core/Domain/DomainServices/PricingResult.cs
@@ -0,0 +1,18 @@
+﻿using BuildingBlocks.Domain;
+
+namespace Domain.DomainServices
+{
+    public class PricingResult
+    {
+        public Money BaseAmount { get; }
+        public Money DiscountAmount { get; }
+        public Money FinalAmount { get; }
+
+        public PricingResult(Money baseAmount, Money discountAmount, Money finalAmount)
+        {
+            BaseAmount = baseAmount;
+            DiscountAmount = discountAmount;
+            FinalAmount = finalAmount;
+        }
+    }
+}
